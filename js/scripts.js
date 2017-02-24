f653a7f904ef20038665afffda688ac25708607b
slaughtr <dallas.xiao@gmail.com>
2017-02-24 14:10:42 -0800

    Follow example by adding additional functionality

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,17 @@ function Contact(first, last) {
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
   });
